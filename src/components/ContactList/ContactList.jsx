4e8955a7c86c5cdf10eff4dd1130ce0532a60233
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-04-30 00:00:20 +0300

    update-Contact-list

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,8 +4,9 @@ import styles from "./ContactList.module.css";
 
 const ContactList = () => {
   const contacts = useSelector((state) => state.contacts.items);
-  console.log(contacts);
+
   const selectNameFilter = useSelector((state) => state.filters.name);
+
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(selectNameFilter.toLowerCase())
   );
